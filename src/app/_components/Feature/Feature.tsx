17a51d24d646bcf632b727eb420df368d02067ee
Shu12388y <spl.sp999@gmail.com>
2024-06-08 00:19:41 +0530

    file addeD

---
+++ src/app/_components/Feature/Feature.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+
+const posts =[
+     {
+    category: 'Design',
+    title: '10 Tips for Crafting the Perfect UX Portfolio',
+    description: 'Learn how to showcase your design skills and stand out in a crowded job market.',
+    author: 'Emily Lee',
+    date: '3 April 2023',
+    avatar: 'https://www.uifaces.co/wp-content/uploads/2022/01/uifaces-logo.svg',
+    poster:
+      'https://images.unsplash.com/photo-1573164713714-d95e436ab8d6?ixlib=rb-4.0.3&auto=format&fit=crop&w=1469&q=80',
+  },
+  {
+    category: 'Technology',
+    title: 'The Future of Mobile App Development',
+    description:
+      'Discover the latest trends and techniques that will shape the future of mobile app development.',
+    author: 'John Smith',
+    date: '1 April 2023',
+    avatar: 'https://randomuser.me/api/portraits/men/32.jpg',
+    poster:
+      'https://images.unsplash.com/photo-1618761714954-0b8cd0026356?ixlib=rb-4.0.3&auto=format&fit=crop&w=1170&q=80',
+  },
+ 
+]
+
+function Feature() {
+  return (
+    <>
+    <section className='flex flex-warp w-[75rem]  gap-10 '>
+        {posts.map((post,index)=>{
+            return(
+                <>
+                 <div key={post.title + '-' + index} className="border">
+              <img src={post.poster} className="aspect-video w-full rounded-md" alt="" />
+              <div className="min-h-min p-3">
+                <p className="mt-4 w-full text-xs font-semibold leading-tight text-gray-700">
+                  #{post.category}
+                </p>
+                <p className="mt-4 flex-1 text-base font-semibold text-gray-900">{post.title}</p>
+                <p className="mt-4 w-full text-sm leading-normal text-gray-600">
+                  {post.description}
+                </p>
+                <div className="mt-4 flex space-x-3 ">
+                  <img className="h-full w-10 rounded-lg" src={post.avatar} alt={post.author} />
+                  <div>
+                    <p className="text-sm font-semibold leading-tight text-gray-900">
+                      {post.author}
+                    </p>
+                    <p className="text-sm leading-tight text-gray-600">{post.date}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+                </>
+            )
+        })}
+    </section>
+    </>
+  )
+}
+
+export default Feature
